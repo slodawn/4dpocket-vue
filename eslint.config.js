7877a5b25646d208b73d495209b9e99a76a148da
zhaolf <zhao.lingfei@qq.com>
2025-10-03 18:08:45 +0800

    chore: add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import * as parserVue from 'vue-eslint-parser'
+import * as parserTypeScript from '@typescript-eslint/parser'
+
+export default [
+  js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: parserVue,
+      parserOptions: {
+        parser: parserTypeScript,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      // Vue rules
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+      'vue/require-default-prop': 'off',
+      'vue/require-explicit-emits': 'off',
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always',
+            normal: 'always',
+            component: 'always',
+          },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
+
+      // TypeScript rules
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      // General rules
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    },
+  },
+  {
+    ignores: ['node_modules', 'dist', 'dist-ssr', '.husky'],
+  },
+]
+
+
